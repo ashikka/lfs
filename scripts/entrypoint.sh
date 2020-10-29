a2d9551da0d2fa0fc9b8c66f3ae05936fc839880
Rahil Kabani <rahil.kabani.4@gmail.com>
2020-10-29 14:48:08 +0530

    fix: folder name

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,4 +2,4 @@ source /root/.bashrc
 bash -ex /scripts/chapter-05/chapter-05.sh
 bash -ex /scripts/chapter-06/chapter-06.sh
 bash -ex /scripts/chapter-07/chapter-07.sh
-bash -ex /scripts/chapter-07/chapter-08.sh
+bash -ex /scripts/chapter-08/chapter-08.sh
