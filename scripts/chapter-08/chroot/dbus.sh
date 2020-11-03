9ae73bc49c71cd303df6a4552ba5bcb499d67bd2
alias-rahil <rahil.kabani.4@gmail.com>
2020-11-04 00:15:31 +0530

    fix: idk what i am doing anymore

--- scripts/chapter-08/chroot/dbus.sh
+++ scripts/chapter-08/chroot/dbus.sh
@@ -13,5 +13,5 @@ make install
 mv /usr/lib/libdbus-1.so.* /lib
 ln -sf ../../lib/$(readlink /usr/lib/libdbus-1.so) /usr/lib/libdbus-1.so
 ln -sf /etc/machine-id /var/lib/dbus
-# sed -i 's:/var/run:/run:' /lib/systemd/system/dbus.socket
+sed -i 's:/var/run:/run:' /lib/systemd/system/dbus.socket
 rm -rf /sources/dbus-1.12.20
